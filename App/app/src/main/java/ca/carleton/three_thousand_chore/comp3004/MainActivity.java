e86e5c37ddc8f753e8f8dc923e5f36f3dc53b3d6
Jack McCracken <jack.mccracken@shopify.com>
2017-09-11 15:32:10 -0400

    Add blank Android Studio project

---
+++ App/app/src/main/java/ca/carleton/three_thousand_chore/comp3004/MainActivity.java
@@ -0,0 +1,13 @@
+package ca.carleton.three_thousand_chore.comp3004;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
