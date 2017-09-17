3e45e7b92b4b7efd696449cba17873ab49cefca0
Jack McCracken <jack.mccracken@shopify.com>
2017-09-17 02:38:36 -0400

    Don't call yourself a nerd...

--- slack_bot/lita-cuslacking/lib/lita/handlers/cuslacking.rb
+++ slack_bot/lita-cuslacking/lib/lita/handlers/cuslacking.rb
@@ -8,7 +8,9 @@ module Lita
           if response.private_message?
             response.reply "You're the only nerd here..."
           else
-            users = robot.roster(response.room).map { |user_id| "@#{User.find_by_id(user_id).mention_name}" }.reject {|user_name| user_name == "@cuhacking"}
+            users = robot.roster(response.room).map { |user_id| "@#{User.find_by_id(user_id).mention_name}" }
+            users = users.reject { |user_name| user_name == "@cuslacking" }
+
             response.reply "Sup nerds #{users.join(' ')}!"
           end
       end
