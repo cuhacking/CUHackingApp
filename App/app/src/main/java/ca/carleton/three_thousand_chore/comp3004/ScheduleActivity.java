bc1d5efe76839ab0973bdfa40560d1b42f90553e
Esti Tweg <estictweg@hotmail.com>
2017-10-05 13:15:13 -0400

    Made activities and xmls for Notificaitons, schedule, map, links, sponsors and request help pages. Added temporary buttons to main activity to bring you to each new activity.

---
+++ App/app/src/main/java/ca/carleton/three_thousand_chore/comp3004/ScheduleActivity.java
@@ -0,0 +1,21 @@
+package ca.carleton.three_thousand_chore.comp3004;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class ScheduleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
