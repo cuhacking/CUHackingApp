305f13683370b1f5286394a9fe020bece44982f8
Esti Tweg <estictweg@hotmail.com>
2017-10-09 18:24:55 -0400

    Add Martello to sponsors activity

--- App/app/src/main/java/ca/carleton/three_thousand_chore/comp3004/SponsorsActivity.java
+++ App/app/src/main/java/ca/carleton/three_thousand_chore/comp3004/SponsorsActivity.java
@@ -1,11 +1,15 @@
 package ca.carleton.three_thousand_chore.comp3004;
 
+import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 
 public class SponsorsActivity extends AppCompatActivity {
 
@@ -16,6 +20,19 @@ public class SponsorsActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        ImageView martelloImageView = (ImageView) findViewById(R.id.martello);
+
+        martelloImageView.setOnClickListener(martelloClickListener);
+
     }
 
+
+    private View.OnClickListener martelloClickListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            //TODO get url from backend
+//            Intent intent = new Intent(Intent.ACTION_VIEW,
+//                    Uri.parse("url"));
+//            startActivity(intent);
+        }
+    };
 }
