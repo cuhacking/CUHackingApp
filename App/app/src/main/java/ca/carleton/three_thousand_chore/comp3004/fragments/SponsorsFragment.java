372e138479a0cb5c05165f2baa31f1eead4bb80d
Elisa Kazan <elisa.kazan@gmail.com>
2017-10-09 22:23:31 -0400

    Created hamburger menu and fragment templates

---
+++ App/app/src/main/java/ca/carleton/three_thousand_chore/comp3004/fragments/SponsorsFragment.java
@@ -0,0 +1,10 @@
+package ca.carleton.three_thousand_chore.comp3004.fragments;
+
+import android.app.Fragment;
+
+/**
+ * Created by elisakazan on 2017-10-09.
+ */
+
+public class SponsorsFragment extends Fragment {
+}
