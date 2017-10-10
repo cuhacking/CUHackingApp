ceaf6902f15ff1c1ec160f6b12566e8382d4a115
Jack McCracken <jack.mccracken@shopify.com>
2017-10-09 20:29:27 -0400

    Add user controller

--- backend/db/migrate/20170928162857_create_users.rb
+++ backend/db/migrate/20170928162857_create_users.rb
@@ -1,6 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
-    create_table :users do |t|  
+    create_table :users do |t|
       t.string :name
 
       t.timestamps
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 20170928164949) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
