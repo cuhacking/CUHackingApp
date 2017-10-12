66210e140d26a29bc425fdb81a26fa20d3511c8e
Esti Tweg <estictweg@hotmail.com>
2017-10-12 12:48:31 -0400

    Added request helper class, internet permissions,

---
+++ App/app/src/main/java/ca/carleton/three_thousand_chore/comp3004/RequestHelper.java
@@ -0,0 +1,61 @@
+package ca.carleton.three_thousand_chore.comp3004;
+
+import android.content.Context;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by estitweg on 2017-10-12.
+ */
+
+public class RequestHelper {
+
+    interface CompletionNotifier{
+        public void requestCompleted(JSONObject response);
+    }
+
+    public void sendRequest(String url, Context content, final CompletionNotifier notifier) {
+        RequestQueue queue = Volley.newRequestQueue(content);
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String res) {
+                        try {
+                            JSONObject response = new JSONObject(res);
+                            notifier.requestCompleted(response);
+                        }
+                        catch (Exception ex) {
+                            ex.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("Accept", "application/json");
+                return params;
+            }
+        };
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+}
