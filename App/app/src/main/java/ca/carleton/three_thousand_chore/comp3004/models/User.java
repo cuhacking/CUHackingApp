da9503f17a23e42b942d49947dbecf9ad927ceb1
Elisa Kazan <elisa.kazan@gmail.com>
2017-10-23 17:53:58 -0400

    Adding token variable

--- App/app/src/main/java/ca/carleton/three_thousand_chore/comp3004/models/User.java
+++ App/app/src/main/java/ca/carleton/three_thousand_chore/comp3004/models/User.java
@@ -3,6 +3,7 @@ package ca.carleton.three_thousand_chore.comp3004.models;
 import android.util.Log;
 
 import com.android.volley.Request;
+import com.google.firebase.iid.FirebaseInstanceId;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -17,6 +18,7 @@ import ca.carleton.three_thousand_chore.comp3004.RequestHelper;
 public class User {
     private int id;
     private String name;
+    private String token;
 
     public User(int id, int[] helpRequests, String name) {
         this.id = id;
@@ -34,15 +36,34 @@ public class User {
         return name;
     }
 
-    public static void createUser(final JsonRequest.CompletionHandler<User> handler) {
-        JsonRequest<User> request = new JsonRequest<>(Request.Method.POST, RequestHelper.BASE_URL + "/users", null, new JsonRequest.ObjectCreationHandler<User>() {
-            @Override
-            public User fromJson(JSONObject obj) throws JSONException {
-                return new User(obj.getInt("id"));
-            }
-        }, handler);
-        RequestHelper rh = RequestHelper.getInstance();
+    public void setToken(String token) {
+        this.token = token;
+    }
 
-        rh.getQueue().add(request);
+    public static void createUser(final JsonRequest.CompletionHandler<User> handler) {
+        try
+        {
+            // Get the token
+            JSONObject params = new JSONObject();
+            params.put("token", FirebaseInstanceId.getInstance().getToken());
+
+            // Make request
+            JsonRequest<User> request = new JsonRequest<>(Request.Method.POST, RequestHelper.BASE_URL + "/users", params, new JsonRequest.ObjectCreationHandler<User>() {
+                @Override
+                public User fromJson(JSONObject response) throws JSONException {
+                    User newuser = new User(response.getInt("id"));
+                    newuser.setToken(response.getString("token"));
+                    return newuser;
+                }
+            }, handler);
+            RequestHelper rh = RequestHelper.getInstance();
+
+            rh.getQueue().add(request);
+        } catch (JSONException e)
+        {
+            e.printStackTrace();
+        }
+
+
     }
 }
