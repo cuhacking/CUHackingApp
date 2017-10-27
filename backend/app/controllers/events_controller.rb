fe6f2f0df3a3cb8111799058b5394ab81f100976
Angelonfira <wizcardforest@gmail.com>
2017-10-27 13:59:44 -0400

    Event to Events

--- backend/app/controllers/events_controller.rb
+++ backend/app/controllers/events_controller.rb
@@ -1,18 +1,18 @@
 class EventsController < ApplicationController
   def index
-    @event = Event.all
+    @events = Event.all
 
     respond_to do |format|
-      format.json { render json: @event.to_json }
+      format.json { render json: @events.to_json }
       format.html
     end
   end
 
   def show
-    @event = Event.all
+    @events = Event.all
 
     respond_to do |format|
-      format.json { render json: @event.to_json }
+      format.json { render json: @events.to_json }
       format.html
     end
   end
