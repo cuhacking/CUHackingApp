fcac467d3dbc0377fc3955972d3cc1d5744043f1
Jack McCracken <jack.mccracken@shopify.com>
2017-11-11 18:39:29 -0500

    Add to seeds

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -6,28 +6,26 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-if Rails.env.development?
-	forest = User.new(name: "Forest")
+forest = User.new(name: "Forest")
 
-	help = HelpRequest.new(user: forest, problem: "Need help with ruby")
+help = HelpRequest.new(user: forest, problem: "Need help with ruby")
 
-	company = Company.new(name: "Spotify", tier: "Gold", website_url: "https://open.spotify.com/browse", logo: "http://www.scdn.co/i/_global/open-graph-default.png")
+company = Company.new(name: "Spotify", tier: "Gold", website_url: "https://open.spotify.com/browse", logo: "http://www.scdn.co/i/_global/open-graph-default.png")
 
-	building = Building.new(name: "Herzberg", initials: "HP")
+building = Building.new(name: "Herzberg", initials: "HP")
 
-	room = Room.new(room_type: "Chill", name: "Comp Sci Lounge", building: building)
+room = Room.new(room_type: "Chill", name: "Comp Sci Lounge", building: building)
 
-	forest.save!
-	help.save!
-	company.save!
-	room.save!
-	building.save!
-	
-	event = Event.new(name: "3004 Meeting", event_type: "Meeting", description: "3004 Meeting", room: room, company: company, date: Date.parse('2017-04-20'), start_time: Time.parse("00:04:20"), end_time: Time.parse("00:06:20"))
-	event2 = Event.new(name: "3004 Meeting, but Jack just arrived", event_type: "Meeting", description: "3004 Meeting but now Jack is here", room: room, company: company, date: Date.parse('2017-04-20'), start_time: Time.parse("00:04:30"), end_time: Time.parse("00:06:30"))
+forest.save!
+help.save!
+company.save!
+room.save!
+building.save!
 
-	event.save!
-	event2.save!
+event = Event.new(name: "3004 Meeting", event_type: "Meeting", description: "3004 Meeting", room: room, company: company, date: Date.parse('2017-04-20'), start_time: Time.parse("00:04:20"), end_time: Time.parse("00:06:20"))
+event2 = Event.new(name: "3004 Meeting, but Jack just arrived", event_type: "Meeting", description: "3004 Meeting but now Jack is here", room: room, company: company, date: Date.parse('2017-04-20'), start_time: Time.parse("00:04:30"), end_time: Time.parse("00:06:30"))
 
-	#notification = 
-end
+event.save!
+event2.save!
+
+#notification = 
