e3f8010fe3d493e9d450b6a448d42137e55cefcd
Jack McCracken <jack.mccracken@shopify.com>
2017-11-11 22:06:47 -0500

    Add migration to make the room name a string

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171031123649) do
+ActiveRecord::Schema.define(version: 20171112030557) do
 
   create_table "buildings", force: :cascade do |t|
     t.string "name"
@@ -20,17 +20,17 @@ ActiveRecord::Schema.define(version: 20171031123649) do
   end
 
   create_table "companies", force: :cascade do |t|
+    t.integer "event_id"
     t.string "name"
     t.string "tier"
     t.string "website_url"
     t.string "logo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_companies_on_event_id"
   end
 
   create_table "events", force: :cascade do |t|
-    t.integer "room_id"
-    t.integer "company_id"
     t.date "date"
     t.time "start_time"
     t.time "end_time"
@@ -39,8 +39,6 @@ ActiveRecord::Schema.define(version: 20171031123649) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.index ["company_id"], name: "index_events_on_company_id"
-    t.index ["room_id"], name: "index_events_on_room_id"
   end
 
   create_table "help_requests", force: :cascade do |t|
@@ -65,12 +63,14 @@ ActiveRecord::Schema.define(version: 20171031123649) do
   end
 
   create_table "rooms", force: :cascade do |t|
+    t.integer "event_id"
     t.integer "building_id"
     t.string "room_type"
-    t.integer "name"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["building_id"], name: "index_rooms_on_building_id"
+    t.index ["event_id"], name: "index_rooms_on_event_id"
   end
 
   create_table "users", force: :cascade do |t|
