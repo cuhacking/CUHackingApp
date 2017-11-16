426e59a6e7ff1907a2407164db47e463805c498a
Esti Tweg <estictweg@hotmail.com>
2017-11-16 12:57:59 -0500

    Add fragment to back stack for schedule detail view to make back button work

--- App/app/src/main/java/ca/carleton/three_thousand_chore/comp3004/fragments/ScheduleFragment.java
+++ App/app/src/main/java/ca/carleton/three_thousand_chore/comp3004/fragments/ScheduleFragment.java
@@ -70,6 +70,7 @@ public class ScheduleFragment extends Fragment {
                         fragment.setArguments(bundle);
                         fragmentManager.beginTransaction()
                                 .replace(R.id.content_frame, fragment)
+                                .addToBackStack(getResources().getStringArray(R.array.activities_array)[1]+"Detail")
                                 .commit();
                     }
                 });
