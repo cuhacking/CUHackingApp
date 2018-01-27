7ee6456177c6477a1796f92712c8da7f771aec97
Jack McCracken <jack.mccracken@shopify.com>
2018-01-27 14:39:30 -0500

    Add view controllers

--- iOS/CUHacking/CUHacking/NotificationsViewController.swift
+++ iOS/CUHacking/CUHacking/NotificationsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  SecondViewController.swift
+//  FirstViewController.swift
 //  CUHacking
 //
 //  Created by Jack McCracken on 2018-01-27.
@@ -8,11 +8,10 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class NotificationsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
--- iOS/CUHacking/CUHacking/RequestHelpViewController.swift
+++ iOS/CUHacking/CUHacking/RequestHelpViewController.swift
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.swift
+//  RequestHelpViewController.swift
 //  CUHacking
 //
 //  Created by Jack McCracken on 2018-01-27.
@@ -8,18 +8,16 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class RequestHelpViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
+
 
