b49cbb195606bf17ce24825e8a0b1df58601b060
Esti Tweg <estictweg@hotmail.com>
2018-02-19 17:27:28 +1030

    added sponsor images in list view

--- iOS/CUHacking/CUHacking/viewcontrollers/SponsorsViewController.swift
+++ iOS/CUHacking/CUHacking/viewcontrollers/SponsorsViewController.swift
@@ -8,7 +8,13 @@
 
 import UIKit
 
-class SponsorsViewController: UIViewController {
+class SponsorCell: UITableViewCell {
+    @IBOutlet weak var sponsorImage: UIImageView!
+}
+
+class SponsorsViewController: UITableViewController {
+    
+    var sponsorImages = [UIImage(named: "cse.jpg")!, UIImage(named: "foko.jpg")!, UIImage(named: "inova.jpg")!, UIImage(named: "kx.jpg")!, UIImage(named: "magmic.jpg")!, UIImage(named: "martello.jpg")!, UIImage(named: "pythian.jpg")!, UIImage(named: "stdlib.jpg")!, UIImage(named: "you.jpg")!]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +24,22 @@ class SponsorsViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sponsorImages.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let sponsorCell = tableView.dequeueReusableCell(withIdentifier: "sponsor_cell") as! SponsorCell
+        
+        sponsorCell.sponsorImage.image = sponsorImages[indexPath.row]
+        
+        return sponsorCell
     }
     
 }
